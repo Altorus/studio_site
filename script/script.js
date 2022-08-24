450ae3af6bbde4d51fa2fec98a0fdeec7b499e7f
Altorus <101462143+Altorus@users.noreply.github.com>
2022-08-24 18:38:02 +0400

    upd_hits

--- script/script.js
+++ script/script.js
@@ -5,3 +5,13 @@ $(document).ready(function(){
         autoplaySpeed: 3000,
     });
 });
+
+$(document).ready(function(){
+    $('.slick-products').slick({
+        dots: true,
+        autoplay: false,
+        autoplaySpeed: 3000,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+    });
+});
