3029cb8fc8ad12e6b2df3cfdbd89cfe975ae38e0
Altorus <101462143+Altorus@users.noreply.github.com>
2022-08-24 19:03:35 +0400

    upd_product

--- script/script.js
+++ script/script.js
@@ -9,9 +9,10 @@ $(document).ready(function(){
 $(document).ready(function(){
     $('.slick-products').slick({
         dots: true,
-        autoplay: false,
+        autoplay: true,
         autoplaySpeed: 3000,
         slidesToShow: 3,
         slidesToScroll: 3,
+        dotsClass: 'products-dots',
     });
 });
