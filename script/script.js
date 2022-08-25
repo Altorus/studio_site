7c43b6f8e6b3f6a6b20e792dff7f08dbbea66f4e
Altorus <101462143+Altorus@users.noreply.github.com>
2022-08-25 11:28:28 +0400

    upd_about

--- script/script.js
+++ script/script.js
@@ -3,16 +3,35 @@ $(document).ready(function(){
         dots: true,
         autoplay: true,
         autoplaySpeed: 3000,
+        arrows:false,
     });
 });
 
 $(document).ready(function(){
     $('.slick-products').slick({
         dots: true,
+        arrows:false,
         autoplay: true,
         autoplaySpeed: 3000,
         slidesToShow: 3,
         slidesToScroll: 3,
         dotsClass: 'products-dots',
+    });
+});
+
+$(document).ready(function(){
+    $('.slick-stock').slick({
+        prevArrow: '<button type = "button" class = "prev-stock"></ button> ',
+        nextArrow: '<button type = "button" class = "next-stock"></ button> ',
+
+        dots: true,
+        dotsClass: 'products-dots',
+        fade:true,
+    });
+});
+
+$(document).ready(function(){
+    $('.slick-about').slick({
+        
     });
 });
