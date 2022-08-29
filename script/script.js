1b94a971b9a031ee2e59496f58182b1d11cbe865
Altorus <101462143+Altorus@users.noreply.github.com>
2022-08-29 23:11:56 +0400

    upd_mobile

--- script/script.js
+++ script/script.js
@@ -1,29 +1,42 @@
 $(document).ready(function() {
     $('.slick').slick({
         dots: true,
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 3000,
         arrows: false,
     });
 });
 
 $(document).ready(function() {
-    $('.slick-products').slick({
-        dots: true,
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        dotsClass: 'products-dots',
-    });
+    if(screen.width > 810){
+        $('.slick-products').slick({
+            dots: true,
+            arrows: false,
+            autoplay: false,
+            autoplaySpeed: 3000,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            dotsClass: 'products-dots',
+        });
+    }
+    else if(screen.width <=810){
+        $('.slick-products').slick({
+            dots: true,
+            arrows: false,
+            autoplay: false,
+            autoplaySpeed: 3000,
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            dotsClass: 'products-dots',
+        });
+    }
 });
 
 $(document).ready(function() {
     $('.slick-stock').slick({
         prevArrow: '<button type = "button" class = "prev-stock"></ button> ',
         nextArrow: '<button type = "button" class = "next-stock"></ button> ',
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 3000,
         dots: true,
         dotsClass: 'products-dots',
@@ -35,7 +48,7 @@ $(document).ready(function() {
     $('.slick-review').slick({
         dots: true,
         arrows: false,
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 3000,
         dotsClass: 'reviews-dots',
     });
@@ -45,7 +58,7 @@ $(document).ready(function() {
     $('.slick-product').slick({
         dots: true,
         arrows: false,
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 3000,
         dotsClass: 'products-dots',
     });
@@ -54,7 +67,7 @@ $(document).ready(function() {
 $(document).ready(function() {
     $('.slick-more-products').slick({
         dots: true,
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 3000,
         slidesToShow: 3,
         slidesToScroll: 3,
@@ -69,7 +82,7 @@ $(document).ready(function() {
         $('.b-popup').toggleClass('b-popup-hiden')
     })
     setTimeout(()=>popupBlock(), 2000)
-    setInterval(()=>popupBlock(), 120000)
+    setInterval(()=>popupBlock(), 300000)
 
     function popupBlock() {
         $('.b-popup').removeClass('b-popup-hiden')
