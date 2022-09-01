803e0c3e54fbb84edd8c03aad5d6feb2f09a630e
Altorus <101462143+Altorus@users.noreply.github.com>
2022-09-01 07:55:20 +0400

    fix

--- script/script.js
+++ script/script.js
@@ -7,6 +7,25 @@ $(document).ready(function() {
     });
 });
 
+$(function() {
+    let header = $('header');
+    let hederHeight = header.height(); // вычисляем высоту шапки
+     
+    $(window).scroll(function() {
+      if($(this).scrollTop() > 1) {
+       header.addClass('header_fixed');
+       $('body').css({
+          'paddingTop': hederHeight+'px' // делаем отступ у body, равный высоте шапки
+       });
+      } else {
+       header.removeClass('header_fixed');
+       $('body').css({
+        'paddingTop': 0 // удаляю отступ у body, равный высоте шапки
+       })
+      }
+    });
+   });
+
 $(document).ready(function() {
 
     if(screen.width > 810){
