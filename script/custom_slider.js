dc67c8edaad62d3ec58a740a75d2d2950f9243f7
Altorus <101462143+Altorus@users.noreply.github.com>
2022-09-04 20:49:17 +0400

    Update custom_slider.js

--- script/custom_slider.js
+++ script/custom_slider.js
@@ -6,21 +6,51 @@ function currentSlide(n) {
 }
 
 $('.mySlides').click(()=>{
-    showSlides(slideIndex=3)
+    prewSlides()
 })
+
+function prewSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length}
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].classList.remove('main');
+        slides[i].classList.add('hidden');
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slideIndex++;
+    if (slideIndex > slides.length-2) { slideIndex = 0 }
+
+    slides[slideIndex + 1].classList.remove('hidden')
+
+    if (slideIndex == slides.length) {
+        slides[0].classList.remove('hidden')
+        slides[0].classList.add('next')
+    } else {
+        slides[slideIndex].classList.remove('hidden')
+        slides[slideIndex].classList.add('next')
+    }
+    console.log(slideIndex);
+
+    slides[slideIndex + 1].classList.remove('next')
+    slides[slideIndex + 1].classList.add('main')
+    dots[slideIndex + 1].className += " active";
+}
 
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
 
-    console.log(slideIndex);
     if (n > slides.length) { slideIndex = 1 }
-
-    console.log(n);
-    if (n < 1) { slideIndex = slides.length
-         console.log(1);
-        }
+    if (n < 1) { slideIndex = slides.length}
 
     for (i = 0; i < slides.length; i++) {
         slides[i].classList.remove('main');
